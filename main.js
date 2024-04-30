c6be44cfadcdfa9b86a612ddbb458f240bdb554d
bossLady0001 <hamzatrukayatabiodun02@gmail.com>
2024-04-29 21:02:11 -0700

    second commit

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ faBar.addEventListener('click',() =>{
     console.log('clicked');
     
 })
+
+// API INTEGRSTION
+const url = 'https://workable.p.rapidapi.com/%7BAPIKEY%7D/jobs?phase=published';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b42e8f0a53mshc2c51fd9d9852f2p1d2977jsn72f57bd023b0',
+		'X-RapidAPI-Host': 'workable.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
